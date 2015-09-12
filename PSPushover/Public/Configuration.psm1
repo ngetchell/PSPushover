6a568da2f122fcfafe9b450125f7cddaf03033b1
Nicholas M. Getchell <getch3028@gmail.com>
2015-09-12 17:56:57 -0400

    Files Added - Appveyor.yml    - A YAML file used with the appveyor service. - configuration.psm1    - A module to handle all of the configuration information.
    Files Modifed
    - PSPushover.psm1
       -  Added the ability to load all private and public sub-modules needed
          for the module
       - Config file storage
    - Manifest file
       - Was causing issues when running pester.

---
+++ PSPushover/Public/Configuration.psm1
@@ -0,0 +1,27 @@
+﻿$ConfigPath = "$env:appdata\PSGitLab\PSPushoverConfiguration.xml"
+
+Function Save-PushoverAPIInformation {
+[cmdletbinding()]
+param(
+    
+    [parameter(Mandatory=$True)]
+    [ValidateNotNull()]
+    [string]$UserKey,
+    
+    [parameter(Mandatory=$True)]
+    [ValidateNotNull()]
+    [string]$AppToken
+
+)
+
+    $ReturnObject = New-Object -TypeName psobject -Property @{
+        UserKey=$UserKey
+        AppToken=$AppToken
+    }
+    Write-Verbose "Saving Pushover API information to $ConfigPath"
+    if (-not (Test-Path (Split-Path $ConfigPath))) {
+        New-Item -ItemType Directory -Path (Split-Path $ConfigPath) | Out-Null
+    }
+    $ReturnObject | Export-clixml -Path $ConfigPath
+
+}
