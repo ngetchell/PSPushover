1cc509019c7f3d9b11f009e7123fa40d6401a114
Nicholas M. Getchell <getch3028@gmail.com>
2016-02-18 11:12:46 -0500

    Added pester.tests.ps1 for each function and beefed up the generic tests pester test. Also deleted a redundant test.

---
+++ Tests/Get-PushoverUserDevice.Tests.ps1
@@ -0,0 +1,23 @@
+﻿$ModuleName = Split-Path (Resolve-Path "$PSScriptRoot\..\" ) -Leaf
+$ModuleManifest = Resolve-Path "$PSScriptRoot\..\$ModuleName\$ModuleName.psd1"
+
+Import-Module $ModuleManifest
+
+InModuleScope $ModuleName {
+    Describe 'Get-PushoverUserDevice' {
+
+        Context 'Mocking Functions' { 
+            It 'Mocking Invoke-RestMethod' { 
+                #Mock Import-PushoverConfig {}
+                Mock Invoke-RestMethod {}
+
+                $Results = Get-PushoverUserDevice 
+
+                #Assert-MockCalled Import-PushoverConfg 1
+                Assert-MockCalled Invoke-RestMethod 1
+            }
+        }
+    }
+}
+
+Remove-Module $ModuleName
